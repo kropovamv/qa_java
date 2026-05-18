c633f9c0f42817940b569abdfe034577832e0891
Kropovamv <kropovamv@yandex.ru>
2026-05-18 22:19:45 +0700

    Добавлен проект, исправлен pom, добавлен класс льва Алекса

---
+++ src/main/java/com/example/AlexLion.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AlexLion extends Lion{
+    public AlexLion() throws Exception {
+        // Передаём "Самец" в конструктор родительского класса Lion
+        super("Самец");
+    }
+
+    public List<String> getFriends() {
+        return Arrays.asList("Марти", "Глория", "Мелман");
+    }
+
+    public String getPlaceOfLiving() {
+        return "Нью-Йоркский зоопарк";
+    }
+
+    @Override
+    public int getKittens() {
+        return 0;
+    }
+}
